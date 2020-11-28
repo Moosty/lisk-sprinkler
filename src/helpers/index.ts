a3e6533dd68da0c4903768fee4b5deb23777758e
corbifex <sander@moosty.com>
2020-11-28 12:54:35 +0100

    v5

--- src/helpers/index.ts
+++
@@ -1,1 +0,0 @@
-export { sprinkler } from './transaction';
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,7 @@
-import { Sprinkler } from './sprinkler';
-import * as Schemas from './schemas';
-import * as Interfaces from './interfaces';
-import * as Constants from './constants';
-import * as client from './helpers';
-
-const configureSprinklerTransaction = (options): any => {
-    return class SprinklerTransaction extends Sprinkler {
-        constructor(props) {
-            super(props, options);
-        }
-    }
-};
-
-const SprinklerTransaction = configureSprinklerTransaction({
-    amount: "10000000000",
-    balance: "100000000",
-});
+import {SprinklerTransaction} from './sprinkler_transaction';
+import {SprinklerModule} from './sprinkler_module';
 
 export {
-    SprinklerTransaction,
-    configureSprinklerTransaction as unConfiguredSprinklerTransaction,
-    Schemas,
-    Interfaces,
-    Constants,
-    client,
+  SprinklerTransaction,
+  SprinklerModule,
 }
