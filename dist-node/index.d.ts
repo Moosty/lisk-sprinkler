7c6223f4f523ee924de7b59c33567123e7ce79e0
corbifex <sander@moosty.com>
2021-04-22 16:46:20 +0200

    star upgrading 5.0.4

--- dist-node/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { SprinklerTransaction } from './sprinkler_transaction';
-import { SprinklerModule } from './sprinkler_module';
-export { SprinklerTransaction, SprinklerModule, };
--- dist-node/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SprinklerModule = exports.SprinklerTransaction = void 0;
-const sprinkler_transaction_1 = require("./sprinkler_transaction");
-Object.defineProperty(exports, "SprinklerTransaction", { enumerable: true, get: function () { return sprinkler_transaction_1.SprinklerTransaction; } });
-const sprinkler_module_1 = require("./sprinkler_module");
-Object.defineProperty(exports, "SprinklerModule", { enumerable: true, get: function () { return sprinkler_module_1.SprinklerModule; } });
-//# sourceMappingURL=index.js.map
